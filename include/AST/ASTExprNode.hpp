100a79f81d398d82cfa7f807a857886f7ce0fcef
Emilien Lemaire <emilien.lem@icloud.com>
2020-09-13 20:07:12 +0200

    Add ASTExpr headers

--- include/AST/ASTExprNode.hpp
+++ include/AST/ASTExprNode.hpp
@@ -1,11 +1,66 @@
 #pragma once
 
+#include <memory>
+#include <vector>
 class ASTExprNode
-{
-private:
-    
+{};
 
+template<typename T>
+class ASTLiteralNode: public ASTExprNode {
+private:
+    T m_Value;
 public:
-    ASTExprNode();
-    virtual ~ASTExprNode();
+    ASTLiteralNode(T value)
+        : m_Value(value)
+    {}
 };
+
+class ASTBinaryNode: public ASTExprNode {
+private:
+    std::unique_ptr<ASTExprNode> m_LeftOperrand;
+    char m_Operator;
+    std::unique_ptr<ASTExprNode> m_RightOperrand;
+public:
+    ASTBinaryNode(
+            std::unique_ptr<ASTExprNode> leftOperrand,
+            char t_Operator,
+            std::unique_ptr<ASTExprNode> rightOperrand
+            );
+};
+
+class ASTIdentifierNode: public ASTExprNode {
+private:
+    std::string m_Identifier;
+public:
+    ASTIdentifierNode(std::string identifier);
+};
+
+class ASTNamspaceIdentifierNode: public ASTExprNode {
+private:
+    std::string m_Namespace;
+    std::string m_Identifier;
+public:
+    ASTNamspaceIdentifierNode(std::string t_Namespace, std::string identifier);
+};
+
+class ASTFunctionCallNode: public ASTExprNode {
+private:
+    std::string m_Name;
+    std::vector<std::unique_ptr<ASTExprNode>> m_Args;
+public:
+    ASTFunctionCallNode(std::string name, std::vector<std::unique_ptr<ASTExprNode>> args);
+};
+
+class ASTMethodCallNode: public ASTExprNode {
+private:
+    std::string m_StructIdentifier;
+    std::string m_MethodName;
+    std::vector<std::unique_ptr<ASTExprNode>> m_Args;
+public:
+    ASTMethodCallNode(
+            std::string structIdentifier,
+            std::string methodName,
+            std::vector<std::unique_ptr<ASTExprNode>> args
+            );
+};
+
